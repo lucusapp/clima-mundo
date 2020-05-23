dccfb66641e9ff4495702a17f61627706a826465
Vi Rey <gavialus@hotmail.com>
2020-05-23 08:15:08 +0000

    57. Optimizando el código coordenadas

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 
-const axios = require ('axios')
-
+const lugar = require ('./lugar/lugar')
 const argv = require ('yargs').options({
     direccion:{
         alias: 'd',
@@ -10,19 +9,5 @@ const argv = require ('yargs').options({
 }).argv;
 
 
-const encodedUrl = encodeURI (argv.direccion)
-console.log(encodedUrl)
-
-
-const instance = axios.create({
-    baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encodedUrl}`,
-    headers: {'X-RapidAPI-Key': '53f8d92fe9msh6ea63c0916fab12p1e52afjsn9971ea949402'}
-  });
-
-  instance.get()
-        .then(resp=>{
-            console.log(resp.data.Results[0])
-        })
-        .catch(err=>{
-            console.log('Error!!',err)
-        })
+lugar.getLugarLatLng(argv.direccion)
+    .then(console.log)
